9b6ef35690ecc0eb14132a6d43057e8155825d21
Christopher Cohen <ender9889@gmail.com>
2022-12-20 23:01:01 -0800

    randomize splash page

--- react-app/src/components/PostIndex/PostIndex.js
+++ react-app/src/components/PostIndex/PostIndex.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { getPosts } from "../../store/all_posts";
@@ -7,10 +7,14 @@ import './PostIndex.css';
 
 const PostIndex = () => {
     const dispatch = useDispatch();
-    const allPosts = useSelector(state => Object.values(state.allPosts))
+    const [allPosts, setAllPosts] = useState([]);
 
     useEffect(() => {
-        dispatch(getPosts());
+        (async function fetchProducts() {
+            const allPosts = await dispatch(getPosts());
+            const shuffled = allPosts.Posts.sort(() => 0.5 - Math.random());
+            setAllPosts(shuffled);
+        })()
     }, [dispatch])
 
     if (!allPosts || !allPosts.length) return null;
@@ -33,16 +37,16 @@ const PostIndex = () => {
                             <h5 className='side-featured-user'>{allPosts[1].postOwner.username}</h5>
                             <h1 className='side-featured-title'>{allPosts[1].title}</h1>
                         </span>
-                        <img className='featured-img' src={allPosts[1].postImages[allPosts[1].previewImgId].url} alt="" />
+                        <img className='side-featured-img' src={allPosts[1].postImages[allPosts[1].previewImgId].url} alt="" />
                     </NavLink>
                 </div>
-                <div className='main-post'>
+                <div className='main-post bottom'>
                     <NavLink to={`/posts/${allPosts[2].id}`}>
                         <span className="side-featured-user-and-title">
                             <h5 className='side-featured-user'>{allPosts[2].postOwner.username}</h5>
                             <h1 className='side-featured-title'>{allPosts[2].title}</h1>
                         </span>
-                        <img className='featured-img' src={allPosts[2].postImages[allPosts[2].previewImgId].url} alt="" />
+                        <img className='side-featured-img' src={allPosts[2].postImages[allPosts[2].previewImgId].url} alt="" />
                     </NavLink>
                 </div>
             </div>
