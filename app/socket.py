8bf101d0a768d7a92b9264429dfab8000ced4ba0
Christopher Cohen <ender9889@gmail.com>
2023-01-05 12:11:58 -0800

    attempting to implement socket.io rooms

--- app/socket.py
+++ app/socket.py
@@ -1,4 +1,4 @@
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room,send
 import os
 
 
@@ -18,4 +18,21 @@ socketio = SocketIO(cors_allowed_origins=origins)
 # handle chat messages
 @socketio.on("chat")
 def handle_chat(data):
-    emit("chat", data, broadcast=True)
+    if data['room']:
+        room = data['room']
+        emit("chat", data, broadcast=True, to=room)
+
+@socketio.on('join')
+def on_join(data):
+    username = data['user']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['user']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
