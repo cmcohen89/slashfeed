f0cd32d8f77548bed17c80e0af02f0780988c1af
Christopher Cohen <ender9889@gmail.com>
2023-01-07 13:36:34 -0800

    search feature endpoint

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request, redirect, render_template
-from app.models import db, Product
+from app.models import db, User
 from sqlalchemy import or_
 
 search_routes = Blueprint("search", __name__)
@@ -10,7 +10,7 @@ def search(query):
     Query for searching all products
     """
 
-    formatted_query = " ".join(query.split('+'))
-    queried_products = Product.query.filter(or_(Product.title.ilike(f"%{formatted_query}%"), Product.detailed_description.ilike(f"%{formatted_query}%"))).all()
+    # formatted_query = " ".join(query.split('+'))
+    # queried_products = Product.query.filter(or_(Product.title.ilike(f"%{formatted_query}%"), Product.detailed_description.ilike(f"%{formatted_query}%"))).all()
 
-    return {"query": [product.to_dict() for product in queried_products]}
+    # return {"query": [product.to_dict() for product in queried_products]}
